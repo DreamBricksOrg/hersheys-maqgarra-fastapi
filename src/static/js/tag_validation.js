1601eaeb74e5bb95205a63a2a464d95806172dbb
VitorHugo1901 <vhms48@gmail.com>
2026-03-18 16:53:07 -0300

    feat: changed the visuals of the tag_validation and added a button to start scanning

--- src/static/js/tag_validation.js
+++ src/static/js/tag_validation.js
@@ -1,9 +1,15 @@
 var html5QrCode = null;
+const qrScanContainer = document.getElementById("Qr-reader-container")
+const btn_scan = document.getElementById("btn_scan")
+const modal_alert = document.getElementById("modal_alert")
+const modal_success = document.getElementById("modal_success")
+const modal_error = document.getElementById("modal_error")
+const tbody_element = document.getElementById('queue_table').getElementsByTagName('tbody')[0];
 
+btn_scan.addEventListener("click", function () {
+    startScan();
+})
 function onScanSuccess(decodedText, decodedResult) {
-    const modal_alert = document.getElementById("modal_alert")
-    const modal_success = document.getElementById("modal_success")
-    const modal_error = document.getElementById("modal_error")
     // handle the scanned code as you like, for example:
     console.log("Code matched = ${decodedText}", decodedResult);
     fetch(`/api/tags/${decodedText}`, {
@@ -28,13 +34,13 @@ function onScanSuccess(decodedText, decodedResult) {
         else {
             modal_error.style.display = "flex";
             setTimeout(() => { hideElement(modal_error) }, 2000);
-
         }
     }).catch(error => {
         modal_alert.style.display = "flex";
         setTimeout(() => { hideElement(modal_alert) }, 2000);
     });
     html5QrCode.stop();
+    btn_scan.style.display = "flex"
 }
 
 async function deactivateTag(tag_key) {
@@ -54,6 +60,7 @@ async function deactivateTag(tag_key) {
 }
 
 async function startScan() {
+    btn_scan.style.display = "none"
     html5QrCode = new Html5Qrcode("reader");
     let qrboxFunction = function (viewfinderWidth, viewfinderHeight) {
         let minEdgeSize = Math.min(viewfinderWidth, viewfinderHeight);
@@ -62,14 +69,29 @@ async function startScan() {
     }
     const config = { fps: 10, qrbox: qrboxFunction };
 
-    // If you want to prefer back camera
     html5QrCode.start({ facingMode: "user" }, config, onScanSuccess);
 }
-document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(startScan(), 3000);
-}, false);
 
+function test_queue() {
+    for (let x = 1; x < 15; x++) {
+        let newRow = tbody_element.insertRow(-1);
+
+        // 3. Insert cells (<td>) into the new row
+        let cell_position = newRow.insertCell(0);
+        let cell_code = newRow.insertCell(1);
+        newRow.insertCell(2).textContent = "-";
+        let cell_tag = newRow.insertCell(3);
+
+        cell_position.textContent = `${x + 1}.`;
+        cell_code.textContent = `1000000${x + 1}`;
+        cell_tag.textContent = `${Math.floor(Math.random() * 10)}`;
+    }
+}
+
+test_queue()
+// 4. Add text or HTML to the cell
+newCell.textContent = 'New Data';
 function hideElement(element) {
     element.style.display = "none";
-    startScan()
+    //startScan()
 }
