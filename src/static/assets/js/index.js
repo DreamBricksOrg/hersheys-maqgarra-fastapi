3e71a851129d76ac84db5db8008b7ccff87013cf
VitorHugo1901 <vhms48@gmail.com>
2026-03-23 16:33:59 -0300

    feat: added manually button and a button to go back to the screen to the more-receipts screen

--- src/static/assets/js/index.js
+++ src/static/assets/js/index.js
@@ -1,5 +1,7 @@
 const btnSim = document.getElementById('btn-sim');
 const btnNao = document.getElementById('btn-nao');
+const btnManual = document.getElementById('btn-manual');
+const btnEnd = document.getElementById('btn-end');
 
 btnSim.addEventListener('click', () => {
     window.location.href = '/pages/scan-qrcode';
@@ -8,3 +10,19 @@ btnSim.addEventListener('click', () => {
 btnNao.addEventListener('click', () => {
     window.location.href = '/pages/read-camera';
 });
+
+btnManual.addEventListener('click', () => {
+    window.location.href = '/pages/add-manually';
+});
+
+btnEnd.addEventListener('click', () => {
+    window.location.href = '/pages/more-receipts';
+});
+
+receiptIds = getReceiptIds();
+if (receiptIds && receiptIds.length > 0) {
+    btnEnd.style.display= "block"
+}
+else{
+    btnEnd.style.display= "none"
+}
